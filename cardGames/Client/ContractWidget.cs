3b20bd02298c1228127882dfa1039fe803dab5d9
Maxime Cauvin <maxime.cauvin@epitech.eu>
2017-11-06 19:16:28 +0100

    MainWindowTemplate(feat): Improvements with the template + ContractWidget done

---
+++ cardGames/Client/ContractWidget.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    class ContractWidget
+    {
+        private Grid contractGrid = new Grid();
+        public Grid ContractGrid {
+            get { return contractGrid; }
+        }
+
+        private ComboBox contractChoice = new ComboBox();
+        public ComboBox ContractChoice  {
+            get { return contractChoice; }
+        }
+
+        private Button contractConfirm = new Button();
+        public Button ContractConfirm {
+            get { return contractConfirm; }
+        }
+
+        public ContractWidget()
+        {
+            contractGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            contractGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            contractGrid.ColumnDefinitions[0].Width = new GridLength(3, GridUnitType.Star);
+            contractGrid.ColumnDefinitions[1].Width = new GridLength(1, GridUnitType.Star);
+
+            contractChoice.Width = 300;
+            contractChoice.Height = 22;
+            contractChoice.Items.Add("Sans atout");
+            contractChoice.Items.Add("Pique");
+            contractChoice.Items.Add("Trefle");
+            contractChoice.Items.Add("Carreau");
+            contractChoice.Items.Add("Coeur");
+            contractChoice.Items.Add("Tout atout");
+            contractChoice.SelectedIndex = 0;
+
+            contractConfirm.Width = 100;
+            contractConfirm.Height = 30;
+            contractConfirm.Content = "Choose";
+
+            Grid.SetColumn(contractChoice, 0);
+            Grid.SetColumn(contractConfirm, 1);
+
+            contractGrid.Children.Add(contractChoice);
+            contractGrid.Children.Add(contractConfirm);
+        }
+    }
+}
--- cardGames/Client/MainWindow.xaml.cs
+++ cardGames/Client/MainWindow.xaml.cs
@@ -20,9 +20,7 @@ namespace Client
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Grid g = new Grid();
-        private ComboBox c = new ComboBox();
-        private Button b = new Button();
+        private ContractWidget contractWidget = new ContractWidget();
 
         public MainWindow()
         {
@@ -38,33 +36,9 @@ namespace Client
             Canvas.SetTop(rect, this.TestCanvas.Height / 2 - rect.Height / 2);
             this.TestCanvas.Children.Add(rect);
 
-            g.ColumnDefinitions.Add(new ColumnDefinition());
-            g.ColumnDefinitions.Add(new ColumnDefinition());
-            g.ColumnDefinitions[0].Width = new GridLength(3, GridUnitType.Star);
-            g.ColumnDefinitions[1].Width = new GridLength(1, GridUnitType.Star);
-
-            c.Width = 300;
-            c.Height = 22;
-            c.Items.Add("Sans atout");
-            c.Items.Add("Pique");
-            c.Items.Add("Trefle");
-            c.Items.Add("Carreau");
-            c.Items.Add("Coeur");
-            c.Items.Add("Tout atout");
-            c.SelectedIndex = 0;
-
-            b.Width = 100;
-            b.Height = 30;
-            b.Content = "Choose";
-
-            Grid.SetColumn(c, 0);
-            Grid.SetColumn(b, 1);
-
-            g.Children.Add(c);
-            g.Children.Add(b);
-
-            BottomActions.Child = g;
-
+            if (contractWidget.ContractGrid != null)
+                Console.Write("rip");
+            BottomActions.Child = contractWidget.ContractGrid;
         }
     }
 }
