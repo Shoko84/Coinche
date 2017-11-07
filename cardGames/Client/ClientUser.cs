d5c9b09e34de12e182c89e9112eb5414cef47dfe
Maxime Cauvin <maxime.cauvin@epitech.eu>
2017-11-08 00:42:28 +0100

    DrawPlayedCards(feat): Displaying played cards done

--- cardGames/Client/ClientUser.cs
+++ cardGames/Client/ClientUser.cs
@@ -8,8 +8,22 @@ namespace Client
 {
     public class ClientUser
     {
+        public enum ClientPosition : int
+        {
+            Bottom = 0,
+            Left = 1,
+            Top,
+            Right
+        }
+
+        private int id;
+        public int Id { get => id; }
+
         private string username;
         public string Username { get => username; }
+
+        private ClientPosition position;
+        public ClientPosition Position { get => position; }
 
         private List<Card> cardsList;
         public List<Card> CardsList { get => cardsList; }
@@ -24,9 +38,11 @@ namespace Client
             // TODO
         }
 
-        public ClientUser(string _username)
+        public ClientUser(int _id, string _username, ClientPosition _position)
         {
+            id = _id;
             username = _username;
+            position = _position;
             cardsList = new List<Card>();
         }
     }
--- cardGames/Client/MainWindow.xaml.cs
+++ cardGames/Client/MainWindow.xaml.cs
@@ -40,6 +40,7 @@ namespace Client
 
         public void DrawGameField()
         {
+            this.TestCanvas.Children.Clear();
             //Background
             DisplayRectOnCanvas(0, 0, this.TestCanvas.Width - this.TestCanvas.Margin.Right - this.TestCanvas.Margin.Left,
                                 this.TestCanvas.Height - this.TestCanvas.Margin.Top - this.TestCanvas.Margin.Bottom,
@@ -81,14 +82,16 @@ namespace Client
                 Uri u = new Uri(p);
                 BitmapImage b = new BitmapImage(u);
 
-                Point spaceTaken;
-                if (i % 2 == 0)
-                    spaceTaken = new Point(cardsList[i].CardsList.Count * (b.Width * 0.70) - (cardsList[i].CardsList.Count - 1) * ((b.Width * 0.70) - 20), b.Height * 0.70);
-                else
-                    spaceTaken = new Point((b.Width * 0.70), cardsList[i].CardsList.Count * (b.Height * 0.70) - (cardsList[i].CardsList.Count - 1) * ((b.Height * 0.70) - 20));
-
                 for (int j = 0; j < cardsList[i].CardsList.Count; j++)
                 {
+                    int posCard = (int)cardsList[i].CardsList[j].Position;
+
+                    Point spaceTaken;
+                    if (posCard % 2 == 0)
+                        spaceTaken = new Point(cardsList[i].CardsList.Count * (b.Width * 0.70) - (cardsList[i].CardsList.Count - 1) * ((b.Width * 0.70) - 20), b.Height * 0.70);
+                    else
+                        spaceTaken = new Point((b.Width * 0.70), cardsList[i].CardsList.Count * (b.Height * 0.70) - (cardsList[i].CardsList.Count - 1) * ((b.Height * 0.70) - 20));
+
                     Image img = new Image();
 
                     string path = System.IO.Path.Combine(Environment.CurrentDirectory, "..", "..", "ressources", "assets", "cards", cardsList[i].CardsList[j].StringColour, cardsList[i].CardsList[j].StringValue + ".png");
@@ -97,31 +100,30 @@ namespace Client
 
                     bmp.BeginInit();
                     bmp.UriSource = uri;
-                    bmp.Rotation = cardsRot[i];
+                    bmp.Rotation = cardsRot[posCard];
                     bmp.EndInit();
 
                     img.Source = bmp;
-                    img.Width = bmp.Width * 0.70; //57.6
-                    img.Height = bmp.Height * 0.70; //76.8
-                    img.RenderTransformOrigin = new Point(0.5, 0.5);
+                    img.Width = bmp.Width * 0.70;
+                    img.Height = bmp.Height * 0.70;
 
-                    switch (i)
+                    switch (posCard)
                     {
                         case 0:
-                            Canvas.SetLeft(img, cardsPos[i].X + (j * 20) - spaceTaken.X / 2);
-                            Canvas.SetTop(img, cardsPos[i].Y - img.Height);
+                            Canvas.SetLeft(img, cardsPos[posCard].X + (j * 20) - spaceTaken.X / 2);
+                            Canvas.SetTop(img, cardsPos[posCard].Y - img.Height);
                             break;
                         case 1:
-                            Canvas.SetLeft(img, cardsPos[i].X);
-                            Canvas.SetTop(img, cardsPos[i].Y + (j * 20) - spaceTaken.Y / 2);
+                            Canvas.SetLeft(img, cardsPos[posCard].X);
+                            Canvas.SetTop(img, cardsPos[posCard].Y + (j * 20) - spaceTaken.Y / 2);
                             break;
                         case 2:
-                            Canvas.SetLeft(img, cardsPos[i].X + (j * 20) - spaceTaken.X / 2);
-                            Canvas.SetTop(img, cardsPos[i].Y);
+                            Canvas.SetLeft(img, cardsPos[posCard].X + (j * 20) - spaceTaken.X / 2);
+                            Canvas.SetTop(img, cardsPos[posCard].Y);
                             break;
                         case 3:
-                            Canvas.SetLeft(img, cardsPos[i].X - img.Width);
-                            Canvas.SetTop(img, cardsPos[i].Y + (j * 20) - spaceTaken.Y / 2);
+                            Canvas.SetLeft(img, cardsPos[posCard].X - img.Width);
+                            Canvas.SetTop(img, cardsPos[posCard].Y + (j * 20) - spaceTaken.Y / 2);
                             break;
                     }
 
@@ -132,7 +134,29 @@ namespace Client
 
         public void DrawCardsPlayed(List<Card> cardsPlayed)
         {
+            Point[] cardsPos = new Point[]
+            {
+                new Point(this.TestCanvas.Width / 2, this.TestCanvas.Height / 2 + 40),
+                new Point(this.TestCanvas.Width / 2 - 40, this.TestCanvas.Height / 2),
+                new Point(this.TestCanvas.Width / 2, this.TestCanvas.Height / 2 - 40),
+                new Point(this.TestCanvas.Width / 2 + 40, this.TestCanvas.Height / 2),
+            };
 
+            for (int i = 0; i < cardsPlayed.Count; i++)
+            {
+                Image img = new Image();
+
+                string path = System.IO.Path.Combine(Environment.CurrentDirectory, "..", "..", "ressources", "assets", "cards", cardsPlayed[i].StringColour, cardsPlayed[i].StringValue + ".png");
+                Uri uri = new Uri(path);
+                BitmapImage bmp = new BitmapImage(uri);
+
+                img.Source = bmp;
+                img.Width = bmp.Width * 0.70;
+                img.Height = bmp.Height * 0.70;
+                Canvas.SetLeft(img, cardsPos[(int)cardsPlayed[i].Position].X - img.Width / 2);
+                Canvas.SetTop(img, cardsPos[(int)cardsPlayed[i].Position].Y - img.Height / 2);
+                this.TestCanvas.Children.Add(img);
+            }
         }
 
         public MainWindow()
@@ -149,22 +173,29 @@ namespace Client
             BottomActions.Child = coincheCallWidget.CoincheGrid;
             BottomActions.Child = ingameCallWidget.IngameCallGrid;
 
-            gameInfos.AddPlayer("Shoko");
-            gameInfos.AddCardToPlayer("Shoko", new Card(Card.CardColour.Spades, Card.CardValue.Ace));
-            gameInfos.AddCardToPlayer("Shoko", new Card(Card.CardColour.Hearts, Card.CardValue.Queen));
-            gameInfos.AddCardToPlayer("Shoko", new Card(Card.CardColour.Spades, Card.CardValue.Ten));
-            gameInfos.AddCardToPlayer("Shoko", new Card(Card.CardColour.Diamonds, Card.CardValue.King));
-            gameInfos.AddCardToPlayer("Shoko", new Card(Card.CardColour.Clubs, Card.CardValue.Four));
-            gameInfos.AddPlayer("Albert");
-            gameInfos.AddCardToPlayer("Albert", new Card(Card.CardColour.Unknown, Card.CardValue.Unknown));
-            gameInfos.AddCardToPlayer("Albert", new Card(Card.CardColour.Unknown, Card.CardValue.Unknown));
-            gameInfos.AddCardToPlayer("Albert", new Card(Card.CardColour.Unknown, Card.CardValue.Unknown));
-            gameInfos.AddPlayer("Marco");
-            gameInfos.AddCardToPlayer("Marco", new Card(Card.CardColour.Unknown, Card.CardValue.Unknown));
-            gameInfos.AddPlayer("Herbaux");
-            gameInfos.AddCardToPlayer("Herbaux", new Card(Card.CardColour.Unknown, Card.CardValue.Unknown));
-            gameInfos.AddCardToPlayer("Herbaux", new Card(Card.CardColour.Unknown, Card.CardValue.Unknown));
-            gameInfos.AddCardToPlayer("Herbaux", new Card(Card.CardColour.Unknown, Card.CardValue.Unknown));
+            gameInfos.AddPlayer(2, "Shoko", true);
+            gameInfos.AddCardToPlayerId(2, new Card(Card.CardColour.Spades, Card.CardValue.Ace, Card.CardPosition.Bottom));
+            gameInfos.AddCardToPlayerId(2, new Card(Card.CardColour.Hearts, Card.CardValue.Queen, Card.CardPosition.Bottom));
+            gameInfos.AddCardToPlayerId(2, new Card(Card.CardColour.Spades, Card.CardValue.Ten, Card.CardPosition.Bottom));
+            gameInfos.AddCardToPlayerId(2, new Card(Card.CardColour.Diamonds, Card.CardValue.King, Card.CardPosition.Bottom));
+            gameInfos.AddCardToPlayerId(2, new Card(Card.CardColour.Clubs, Card.CardValue.Four, Card.CardPosition.Bottom));
+            gameInfos.AddPlayer(0, "Marco", false);
+            gameInfos.AddCardToPlayerId(0, new Card(Card.CardColour.Unknown, Card.CardValue.Unknown, Card.CardPosition.Top));
+            gameInfos.AddPlayer(1, "Herbaux", false);
+            gameInfos.AddCardToPlayerId(1, new Card(Card.CardColour.Unknown, Card.CardValue.Unknown, Card.CardPosition.Right));
+            gameInfos.AddCardToPlayerId(1, new Card(Card.CardColour.Unknown, Card.CardValue.Unknown, Card.CardPosition.Right));
+            gameInfos.AddCardToPlayerId(1, new Card(Card.CardColour.Unknown, Card.CardValue.Unknown, Card.CardPosition.Right));
+            gameInfos.AddCardToPlayerId(1, new Card(Card.CardColour.Unknown, Card.CardValue.Unknown, Card.CardPosition.Right));
+            gameInfos.AddCardToPlayerId(1, new Card(Card.CardColour.Unknown, Card.CardValue.Unknown, Card.CardPosition.Right));
+            gameInfos.AddCardToPlayerId(1, new Card(Card.CardColour.Unknown, Card.CardValue.Unknown, Card.CardPosition.Right));
+            gameInfos.AddPlayer(3, "Albert", false);
+            gameInfos.AddCardToPlayerId(3, new Card(Card.CardColour.Unknown, Card.CardValue.Unknown, Card.CardPosition.Left));
+            gameInfos.AddCardToPlayerId(3, new Card(Card.CardColour.Unknown, Card.CardValue.Unknown, Card.CardPosition.Left));
+            gameInfos.AddCardToPlayerId(3, new Card(Card.CardColour.Unknown, Card.CardValue.Unknown, Card.CardPosition.Left));
+            gameInfos.AddCardsPlayed(new Card(Card.CardColour.Diamonds, Card.CardValue.Six, Card.CardPosition.Right));
+            gameInfos.AddCardsPlayed(new Card(Card.CardColour.Clubs, Card.CardValue.King, Card.CardPosition.Top));
+            gameInfos.AddCardsPlayed(new Card(Card.CardColour.Hearts, Card.CardValue.Jack, Card.CardPosition.Left));
+            gameInfos.AddCardsPlayed(new Card(Card.CardColour.Spades, Card.CardValue.Nine, Card.CardPosition.Bottom));
         }
     }
 }
