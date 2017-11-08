882a1409c9346b0921f41ae8b08831dbde7a9556
Maxime Cauvin <maxime.cauvin@epitech.eu>
2017-11-08 20:17:59 +0100

    TemplateEventFunctions(feat): Connection event template prepared

--- cardGames/Client/LoginContent.xaml.cs
+++ cardGames/Client/LoginContent.xaml.cs
@@ -25,9 +25,41 @@ namespace Client
             InitializeComponent();
         }
 
+        private string SendCredentialsToServer(string username, string ip, string port)
+        {
+            string status = "OK";
+
+            if (string.IsNullOrEmpty(username))
+                status = "Username not specified";
+            else
+            {
+                // TODO: Call a function to send to the server the credentials
+
+                int myId = 2; // Returned by the server
+                // if connection allowed
+                if (true)
+                    GameInfos.Instance.AddPlayer(myId, username, true);
+                else
+                    status = "Could not connect to the server";
+            }
+            return (status);
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            ((MainWindow)App.Current.MainWindow).ContentArea.Content = new WaitingScreenContent();
+            MainWindow win;
+
+            if (App.Current.MainWindow is MainWindow)
+            {
+                string status;
+                if ((status = SendCredentialsToServer(UsernameField.Text, AddressField.Text, PortField.Text)) == "OK")
+                {
+                    win = App.Current.MainWindow as MainWindow;
+                    win.ContentArea.Content = new WaitingScreenContent();
+                }
+                else
+                    MessageBox.Show(status, "Connection error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
         }
     }
 }
